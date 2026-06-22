7340ed2e640325d4903bdef7eea353180036de66
Esley Aury <esleyaury@gmail.com>
2026-06-22 16:14:48 -0300

    create: start VirtualMemory atrib.

--- src/main/java/br/upe/so/memory/VirtualMemorySystem.java
+++ src/main/java/br/upe/so/memory/VirtualMemorySystem.java
@@ -5,15 +5,5 @@ public class VirtualMemorySystem{
   private VirtualMemory virutalMem;
   private PhysicalMemory physicalMem;
 
-
-  private void ocuparFrame(int frame, int pagvirt, int valor){
-    physicalMem.write(frame, valor);
-    physicalMem.ocupar(frame, pagvirt);
-
-    Pagina p = virutalMem.getPagina(pagvirt);
-    p.setPresente(true);
-    p.setNumeroFrame(frame);
-    p.setReferenciado(true);
-    p.setLastUsed(System.currentTimeMillis());
   }
 }
